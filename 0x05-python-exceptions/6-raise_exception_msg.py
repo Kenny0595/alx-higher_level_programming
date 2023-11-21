b3acd2e52fb0c44597f3cc937719383185cf700f
Kenny0595 <122814918+Kenny0595@users.noreply.github.com>
2023-11-22 00:15:01 +0100

    Add files via upload

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 6-raise_exception_msg.py
+# Kenneth Akadu <kennzhood@yahoo.com>
+
+
+def raise_exception_msg(message=""):
+    """Raise a NameError exception with a message."""
+    raise NameError(message)
+
