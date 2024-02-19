4a14caa3288a2738ebf8a8303d672bdd17ae4751
Kenny0595 <122814918+Kenny0595@users.noreply.github.com>
2024-02-19 03:23:20 +0100

    Add files via upload

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const { list } = require('./100-data');
+
+console.log('Initial list:', list);
+
+const newList = list.map((value, index) => value * index);
+
+console.log('Initial List:', initialList);
+
+console.log('New list:', newList);
